96d809ceb44a58a50666eee679f5ffec6e70aae4
andrelugon <andreluiz.lugon@gmail.com>
2025-09-01 19:53:53 -0300

    Resolução do exercício 20 da lista 04

--- material_radioativo.py
+++ material_radioativo.py
@@ -1,1 +1,15 @@
 # Sua solução aqui
+massa = float(input())
+tempo = 0
+for i in range (1000):
+    if massa < 0.5:
+        break
+    tempo = tempo + 50
+    massa = massa / 2
+
+h = tempo // 3600
+resto = tempo % 3600
+m = resto // 60
+s = resto % 60
+print(f"{h}h {m}m {s}s")
+
